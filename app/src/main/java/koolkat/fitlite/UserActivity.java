c705a0dc7d3a4f9bf3e7f50366fabfcc1863a0f8
Kushagra Mangal <kushagra12mangal@gmail.com>
2017-04-14 04:38:04 +0530

    Log Out and My Profile

--- app/src/main/java/koolkat/fitlite/UserActivity.java
+++ app/src/main/java/koolkat/fitlite/UserActivity.java
@@ -1,24 +1,20 @@
 package koolkat.fitlite;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -93,7 +89,15 @@ public class UserActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            Toast.makeText(getApplicationContext(), "Logged out Successfully!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            return true;
+        }
+        if (id == R.id.action_profile) {
+            startActivity(new Intent(getApplicationContext(), Profile.class));
             return true;
         }
 
